01bb283b42fe30348fcd82fd129d42ba294f6121
turasova <134943646+turasova@users.noreply.github.com>
2023-09-28 11:36:58 +0300

    01-gallery

---
+++ js/01-gallery.js
@@ -0,0 +1,62 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+galleryList.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+galleryList.addEventListener('click', handlerClick);
+
+
+function createMarkup(arr) {
+    return arr.map(({preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    ).join('');
+}
+console.dir(galleryList)
+
+
+function handlerClick(evt) {
+    evt.preventDefault();
+   const galleryItem = evt.target.dataset.source;
+  if (!galleryItem) return;
+  instance.element().querySelector('img').src = galleryItem;
+  instance.show();  
+}
+//Модальне вікно
+
+
+const instance = basicLightbox.create(`
+    < class="modal">
+       <img  src=""
+      data-source=""
+      alt=""/> 
+    </div>`
+,
+  {
+    onShow: () => {
+      galleryList.addEventListener('keydown', escPress)
+    },
+    onClose: () => {
+      galleryList.addEventListener('keydown', escPress)
+    },
+  } 
+
+ );
+
+
+function escPress(evt) {
+    const ESC_KEY_CODE = "Escape";
+    const isEscKey = evt.code === ESC_KEY_CODE;
+    if (!isEscKey) return;
+    instance.close();
+}
+
